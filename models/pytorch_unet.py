bf707c53145a8e83376fc1136b6b5347b5c7cd12
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2022-05-14 15:34:58 +0100

    Add files via upload

---
+++ models/pytorch_unet.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding='same'),
+        nn.BatchNorm2d(num_features=out_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, 3, padding='same'),
+        nn.BatchNorm2d(num_features=out_channels),
+        nn.ReLU(inplace=True)
+    ) 
+    
+class UNet(nn.Module):
+
+    def __init__(self, input_channels=1,n_class=1):
+        super().__init__()
+                
+        self.dconv_down1 = double_conv(input_channels, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)        
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
+        
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
+        
+        self.conv_last1 = nn.Conv2d(64, 2, 1)
+        self.activation = torch.nn.Sigmoid()
+        
+        
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+        
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)   
+        
+        x = self.dconv_down4(x)
+        
+        x = self.upsample(x)        
+        x = torch.cat([x, conv3], dim=1)
+        
+        x = self.dconv_up3(x)
+        x = self.upsample(x)        
+        x = torch.cat([x, conv2], dim=1)       
+
+        x = self.dconv_up2(x)
+        x = self.upsample(x)        
+        x = torch.cat([x, conv1], dim=1)   
+        
+        x = self.dconv_up1(x)
+        
+        out = self.conv_last1(x)
+        
+        
+        return self.activation(out)
